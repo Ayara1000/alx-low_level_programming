11a28f387ab18db1ce41e1dfdf5f0f8a28ac6ef9
Ayara Oladipupo <ayaraoladipupo@hotmail.com>
2023-03-17 19:19:25 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,6 +11,7 @@ int main(void)
 	int digit1, digit2;
 
 	for (digit1 = 0; digit1 < 9; digit1++)
+
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
@@ -18,14 +19,11 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
+				continue;	
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
