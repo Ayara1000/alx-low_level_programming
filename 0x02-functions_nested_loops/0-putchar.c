97227d243bbe7e8d64997c417b5727600c7f1fda
Ayara Oladipupo <ayaraoladipupo@hotmail.com>
2023-03-21 01:51:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * main - Prints "Alxstudent" followed by a new line.
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('A');
-	_putchar('l');
-	_putchar('x');
-	_putchar('s');
-	_putchar('t');
+	_putchar('_');
+	_putchar('p');
 	_putchar('u');
-	_putchar('d');
-	_putchar('e');
-	_putchar('n');
 	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
